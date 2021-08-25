4e084435d0d1e6e19b11f816f63552dd9eb97dd4
skeka <shreker240@gmail.com>
2021-08-25 22:54:22 +0300

    upgrade

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import discord
 import os
+from discord.ext import commands
 
-
-
-client = discord.Client()
+client = commands.Bot(command_prefix="-")
 
 
 @client.event
 async def on_ready():
     print('БОТ ЗАПУЩЕН')
 
+
+@client.command()
+async def play(ctx, url: str):
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    await voiceChannel.connect
 
 client.run(str(os.environ.get("TOKEN")))
