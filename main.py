3ea3e5ff503f22e88c8eb0e81b3e96945cca7096
Seregaaa228 <shreker240@gmail.com>
2022-04-19 14:12:53 +0300

    biba
    1231231

--- main.py
+++ main.py
@@ -1,115 +1,40 @@
-import discord
-from discord.opus import OpusNotLoaded
-from discord.utils import get
-from discord.ext import commands
-from youtube_dl import YoutubeDL
-from asyncio import sleep
-import os
-import opuslib
 
-client = commands.Bot(command_prefix='-')
+from decouple import config
+from discord.ext import commands
+import discord
+import validators
+
+
+client = commands.Bot('-')
 
 
 @client.event
 async def on_ready():
-    print('БОТ ГОТОВ')
+    print(f'We have logged in as {client.user}')
 
-
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False'}
-FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
-
-raise OpusNotLoaded()
-
-import ctypes.util
-
-print("ctypes - Find opus:")
-a = ctypes.util.find_library('opus')
-print(a)
-
-print("Discord - Load Opus:")
-b = discord.opus.load_opus(a)
-print(b)
-
-print("Discord - Is loaded:")
-c = discord.opus.is_loaded()
-print(c)
-
+@client.listen()
+async def on_message(message):
+    if message.content.startswith('бузураб'):
+        voice = discord.utils.get(client.voice_clients, guild=message.guild)
+        voice.play(discord.FFmpegPCMAudio('sounds\\4xIdzwZN6UsN.128.mp3'))
 
 @client.command()
-async def play(ctx, music):
-    discord.opus.is_loaded()
-    if not discord.opus.is_loaded():
-        discord.opus.load_opus('libopus.so')
-    global vc
-    global info
-    try:
-        voice_channel = ctx.message.author.voice.channel
+async def ping(ctx):
+	await ctx.channel.send("pong")
+     
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound ):
+        ...      
 
-        voice = get(client.voice_clients, guild=ctx.guild)
-        if voice and voice.is_connected():
-            vc = await voice.move_to(voice_channel)
-        else:
-            vc = await voice_channel.connect()
-    except AttributeError:
-        await ctx.channel.send("ПОЛЬЗОВАТЕЛЬ НЕ В ГОЛОСОВОМ")
+@client.command(pass_context = True , aliases=['p'])
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
 
-    else:
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(music, download=False)
-
-        URL = info['formats'][0]['url']
-
-        vc.play(discord.FFmpegPCMAudio(source=URL, **FFMPEG_OPTIONS))
-        await ctx.channel.send('МУЗЫКА ИГРАЕТ - ' + info.get('title'))
-
-        while vc.is_playing():
-            await sleep(1)
-        if not vc.is_paused():
-            await vc.disconnect()
+@client.command(pass_context = True , aliases=['s'])
+async def stop(ctx):    
+    await ctx.voice_client.disconnect()
 
 
-@client.command()
-async def p(ctx, music):
-    if not discord.opus.is_loaded():
-        discord.opus.load_opus('libopus.so')
-    global vc
-    global info
-    try:
-        voice_channel = ctx.message.author.voice.channel
-
-        voice = get(client.voice_clients, guild=ctx.guild)
-        if voice and voice.is_connected():
-            vc = await voice.move_to(voice_channel)
-        else:
-            vc = await voice_channel.connect()
-    except AttributeError:
-        await ctx.channel.send("ПОЛЬЗОВАТЕЛЬ НЕ В ГОЛОСОВОМ")
-
-    else:
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(music, download=False)
-
-        URL = info['formats'][0]['url']
-
-        vc.play(discord.FFmpegPCMAudio(source=URL, **FFMPEG_OPTIONS))
-        await ctx.channel.send('МУЗЫКА ИГРАЕТ - ' + info.get('title'))
-
-        while vc.is_playing():
-            await sleep(1200)
-        if not vc.is_paused():
-            await vc.disconnect()
-
-
-@client.command()
-async def stop(ctx):
-    voice_client = ctx.guild.voice_client
-    if voice_client:
-        await ctx.channel.send('ОТКЛЮЧАЮСЬ')
-        await sleep(1)
-        await voice_client.disconnect()
-    else:
-        await ctx.channel.send('Я НЕ В КАНАЛЕ')
-
-
-client.run(os.environ.get("TOKEN"))
+client.run(config("TOKEN"))
